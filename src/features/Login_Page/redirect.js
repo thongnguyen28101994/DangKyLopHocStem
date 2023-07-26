c7faa30cf0dab043c511b93da64d6cb4de0ae8d0
thongnguyen2810 <nguyenthanhminhthong@gmail.com>
2023-07-26 12:10:10 +0700

    up code

--- src/features/Login_Page/redirect.js
+++ src/features/Login_Page/redirect.js
@@ -5,16 +5,19 @@ import { CommonApi } from "../../apis/CommonApi";
 const Direction = () => {
   let location = useLocation();
   let history = useHistory();
-  sessionStorage.setItem(
-    "token",
-    decodeURIComponent(location.search.substring(7))
-  );
 
   useEffect(() => {
+    sessionStorage.setItem(
+      "token",
+      decodeURIComponent(location.search.substring(7))
+    );
     CommonApi.getSession().then(async (res) => {
       if (res.statusCode === 200) {
-        localStorage.setItem("Data", JSON.stringify({...res.result, MA_TRUONG: res.result.SchoolId}));      
-        history.replace("/user/loptaphuan"); 
+        localStorage.setItem(
+          "Data",
+          JSON.stringify({ ...res.result, MA_TRUONG: res.result.SchoolId })
+        );
+        history.replace("/user/loptaphuan");
       }
     });
   }, [history]);
